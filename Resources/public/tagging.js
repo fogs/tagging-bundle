586c5d21e5b67c90a83e8c6767e988c932e54ca7
wtrainer <wtrainer@gmail.com>
2014-03-12 15:24:07 +0000

    Update tagging.js
    Since typeahead went to version 0.10.1 TagsManager can't get an ajax source. This was because Bloodhound and Typeahead are separate now. 
    
    The above method properly encapsulates your TagsManager initialization and makes sure Bloodhound gets all the tags from the server and feeds them into each instance of typeahead.
    
    For this to work i have modified widgets.html.twig. Nothing major, just so that the correct functions can be passed and used in the above.

--- Resources/public/tagging.js
+++ Resources/public/tagging.js
@@ -1,21 +1,53 @@
-var tagsManagers = {};
+(function( $ ) {
+ 
+    $.fn.fogsTagsManager = function( options ) {
 
-(function ($) {
-	$(document).ready(function () { 
-		
-		// find all tagsManager input elements 
-		$("input.tagsmanager").each(function() {
-			// initialize the tagsManager
-			tagsManagers[this.id] = $(this).tagsManager(taggingOptions[this.id]['tagsManager']);
-	
+        // Initialises tags manager
+        // Uses taggingOptions[this.id] set in widgets.html.twig
+        // Usage - $("input.tagsmanager").fogsTagsManager();
+        
+        var tagsManagers = {};
+            
+        var tags = null;    
+            
+        return this.each(function() {
+            
+            //use extra extend attribute of true to go deep
+            opts = $.extend( true, {}, $.fn.fogsTagsManager.defaults, taggingOptions[this.id] );
+            
+            // initialize the tagsManager
+            tagsManagers[this.id] = $(this).tagsManager( opts.tagsManager );
+            
 			// initialize the typeahead, if configured
-			if (taggingOptions[this.id]['typeahead']) {
-				$(this).typeahead(taggingOptions[this.id]['typeahead']).on('typeahead:selected', function (e, d) {
-					tagsManagers[this.id].tagsManager("pushTag", d.value);
-				});	
-			}
-			
-		});
-		
-	});
-})(jQuery);
+			if (opts.suggestions)
+            {
+                if (tags == null)
+                {
+                    tags = new Bloodhound( opts.suggestions );    
+                    tags.initialize();
+                }
+                
+                opts.bloodhound.source = tags.ttAdapter();
+                
+                $(this).typeahead(opts.typeahead, opts.bloodhound )	
+                    .on('typeahead:selected', function (e, d) {
+                        tagsManagers[this.id].tagsManager("pushTag", d.name);
+                    });	
+            }
+            
+        });
+    };
+    
+    // Plugin defaults – added as a property on our plugin function.
+    $.fn.fogsTagsManager.defaults = {
+        bloodhound : {
+            name: 'tags',
+            displayKey: 'name'
+        },
+        suggestions : {
+            datumTokenizer: function(d) { return d; },
+            queryTokenizer: function(q) { return q; }
+        }
+    };
+ 
+}( jQuery ));
