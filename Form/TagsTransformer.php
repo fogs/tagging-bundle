96d7c82cecc0ed6457340bdb45cf2bc35cd74bee
sujayjaju <sjaju@indivision.net>
2014-11-18 11:39:48 +0530

    Update TagsTransformer.php
    To handle empty tags on first use

--- Form/TagsTransformer.php
+++ Form/TagsTransformer.php
@@ -21,7 +21,10 @@ class TagsTransformer implements DataTransformerInterface
 
 	public function transform($tags)
 	{
-		return join(', ', $tags->toArray());
+	        if(!is_null($tags) && is_array($tags))
+			    return join(', ', $tags->toArray());
+	
+	        return '';
 	}
 
 	public function reverseTransform($tags)
