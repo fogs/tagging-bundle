30f681961b935dbfa0541258c94c8eca052e4310
cinam <cinam@hotmail.com>
2016-10-08 23:58:36 +0200

    Formatted the code: PSR-2, trailing whitespaces

--- DependencyInjection/FogsTaggingExtension.php
+++ DependencyInjection/FogsTaggingExtension.php
@@ -24,12 +24,11 @@ class FogsTaggingExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        
+
         // adding this to the config of this bundle did not work..
         $container->setParameter('twig.form.resources', array_merge(
-        		$container->getParameter('twig.form.resources'),
-        		array('FogsTaggingBundle:Form:widgets.html.twig')
+                $container->getParameter('twig.form.resources'),
+                array('FogsTaggingBundle:Form:widgets.html.twig')
         ));
     }
-    
 }
